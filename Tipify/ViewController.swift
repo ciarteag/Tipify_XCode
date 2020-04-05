107aefc0c29a6024b099ef28b1c6f7639676e134
Carlos Arteaga <ciarteag@uci.edu>
2020-04-04 19:41:57 -0700

    Initial Commit

---
+++ Tipify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipify
+//
+//  Created by Carlos Arteaga on 4/4/20.
+//  Copyright © 2020 Carlos Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
